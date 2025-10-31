c48f0564a103ddfc3abb5a044ae054760247c94d
Mbergeron7 <164894886+Mbergeron7@users.noreply.github.com>
2025-10-31 12:58:22 -0400

    Update wfm_ticket_portal.py

--- wfm_ticket_portal.py
+++ wfm_ticket_portal.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return "Ticket Portal is live!"
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    if request.method == 'POST':
+        advisor_name = request.form.get('advisor_name')
+        team_lead = request.form.get('team_lead')
+        request_type = request.form.get('request_type')
+        details = request.form.get('details')
+        # You can log or store this data here
+        return render_template('confirmation.html', advisor_name=advisor_name)
+    return render_template('form.html')
